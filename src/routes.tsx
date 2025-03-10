7a98eac23a6d81f175e71811cf261a76c0dfe5b0
devhnry <devthenry@gmail.com>
2025-03-10 13:14:43 +0100

    feat(about): updated route file to take aboutPage component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home.tsx";
-import Pricing from "./pages/Pricing.tsx"; // Ensure correct import
+import Pricing from "./pages/Pricing.tsx";
+import {About} from "./pages/About.tsx"; // Ensure correct import
 
 export const router = createBrowserRouter([
 		{
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
 		{
 				path: "/pricing",
 				element: <Pricing />
+		},
+		{
+				path: "/about",
+				element: <About />
 		}
 ]);
