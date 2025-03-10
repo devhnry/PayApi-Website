2002e7f19df8691dea853968628495cb8c5c1758
devhnry <devthenry@gmail.com>
2025-03-10 21:32:42 +0100

    feat(routes): updated routes.tsx to include the contact page.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home.tsx";
 import Pricing from "./pages/Pricing.tsx";
-import {About} from "./pages/About.tsx"; // Ensure correct import
+import {About} from "./pages/About.tsx";
+import {Contact} from "./pages/Contact.tsx"; // Ensure correct import
 
 export const router = createBrowserRouter([
 		{
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
 		{
 				path: "/about",
 				element: <About />
+		},
+		{
+				path: "/contact",
+				element: <Contact />
 		}
 ]);
